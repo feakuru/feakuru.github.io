90dbf4a2bc1cb86891847ca7a82284d601cf4fd0
Dmitry Orlov <feanarokurufinve@gmail.com>
2018-06-13 20:08:08 +0300

    Played w paddings

--- src/translation.js
+++ src/translation.js
@@ -1,3 +1,14 @@
+// Snippet:
+// {
+//     translate(
+//         {
+//             'ru': '',
+//             'en': ''
+//         },
+//         this.props.lang
+//     )
+// }
+
 export default function translate(obj, lang) {
     return obj[lang];
 }
